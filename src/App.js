ee7df973463bfa74c0e730b90bd456c2a0ee77da
harpinder singh <harpinderbhullar84@gmail.com>
2022-09-17 21:21:55 +0530

    routes

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from './components/navbar/Navbar';
 import Header from './components/header/Header';
 import Footer from './components/footer/Footer';
 import './App.css';
+import RenderRoutes from './routes';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Navbar />
     {/* Header */}
       <Header />
+      <RenderRoutes />
     {/* Footer */}
       <Footer />
   </div>
