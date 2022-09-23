4e6d2642f83a83751bd4e657fd580d035bb46ee2
harpinder singh <harpinderbhullar84@gmail.com>
2022-09-23 22:31:04 +0530

    create common card & data

--- src/components/service/Service.js
+++ src/components/service/Service.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import Card from '../common/card/Card'
+import servicesList from '../../data/service.json';
 
 const Service = () => {
     return (
@@ -11,78 +13,11 @@ const Service = () => {
                         <h1 className="display-4">Excellent Medical Services</h1>
                     </div>
                     <div className="row g-5">
-                        <div className="col-lg-4 col-md-6">
-                            <div className="service-item bg-light rounded d-flex flex-column align-items-center justify-content-center text-center">
-                                <div className="service-icon mb-4">
-                                    <i className="fa fa-2x fa-user-md text-white" />
-                                </div>
-                                <h4 className="mb-3">Emergency Care</h4>
-                                <p className="m-0">Kasd dolor no lorem nonumy sit labore tempor at justo rebum rebum stet, justo elitr dolor amet sit</p>
-                                <a className="btn btn-lg btn-primary rounded-pill" href>
-                                    <i className="bi bi-arrow-right" />
-                                </a>
-                            </div>
-                        </div>
-                        <div className="col-lg-4 col-md-6">
-                            <div className="service-item bg-light rounded d-flex flex-column align-items-center justify-content-center text-center">
-                                <div className="service-icon mb-4">
-                                    <i className="fa fa-2x fa-procedures text-white" />
-                                </div>
-                                <h4 className="mb-3">Operation &amp; Surgery</h4>
-                                <p className="m-0">Kasd dolor no lorem nonumy sit labore tempor at justo rebum rebum stet, justo elitr dolor amet sit</p>
-                                <a className="btn btn-lg btn-primary rounded-pill" href>
-                                    <i className="bi bi-arrow-right" />
-                                </a>
-                            </div>
-                        </div>
-                        <div className="col-lg-4 col-md-6">
-                            <div className="service-item bg-light rounded d-flex flex-column align-items-center justify-content-center text-center">
-                                <div className="service-icon mb-4">
-                                    <i className="fa fa-2x fa-stethoscope text-white" />
-                                </div>
-                                <h4 className="mb-3">Outdoor Checkup</h4>
-                                <p className="m-0">Kasd dolor no lorem nonumy sit labore tempor at justo rebum rebum stet, justo elitr dolor amet sit</p>
-                                <a className="btn btn-lg btn-primary rounded-pill" href>
-                                    <i className="bi bi-arrow-right" />
-                                </a>
-                            </div>
-                        </div>
-                        <div className="col-lg-4 col-md-6">
-                            <div className="service-item bg-light rounded d-flex flex-column align-items-center justify-content-center text-center">
-                                <div className="service-icon mb-4">
-                                    <i className="fa fa-2x fa-ambulance text-white" />
-                                </div>
-                                <h4 className="mb-3">Ambulance Service</h4>
-                                <p className="m-0">Kasd dolor no lorem nonumy sit labore tempor at justo rebum rebum stet, justo elitr dolor amet sit</p>
-                                <a className="btn btn-lg btn-primary rounded-pill" href>
-                                    <i className="bi bi-arrow-right" />
-                                </a>
-                            </div>
-                        </div>
-                        <div className="col-lg-4 col-md-6">
-                            <div className="service-item bg-light rounded d-flex flex-column align-items-center justify-content-center text-center">
-                                <div className="service-icon mb-4">
-                                    <i className="fa fa-2x fa-pills text-white" />
-                                </div>
-                                <h4 className="mb-3">Medicine &amp; Pharmacy</h4>
-                                <p className="m-0">Kasd dolor no lorem nonumy sit labore tempor at justo rebum rebum stet, justo elitr dolor amet sit</p>
-                                <a className="btn btn-lg btn-primary rounded-pill" href>
-                                    <i className="bi bi-arrow-right" />
-                                </a>
-                            </div>
-                        </div>
-                        <div className="col-lg-4 col-md-6">
-                            <div className="service-item bg-light rounded d-flex flex-column align-items-center justify-content-center text-center">
-                                <div className="service-icon mb-4">
-                                    <i className="fa fa-2x fa-microscope text-white" />
-                                </div>
-                                <h4 className="mb-3">Blood Testing</h4>
-                                <p className="m-0">Kasd dolor no lorem nonumy sit labore tempor at justo rebum rebum stet, justo elitr dolor amet sit</p>
-                                <a className="btn btn-lg btn-primary rounded-pill" href>
-                                    <i className="bi bi-arrow-right" />
-                                </a>
-                            </div>
-                        </div>
+                        {
+                            servicesList.map((service)=> 
+                                <Card data={service} />
+                            )
+                        }
                     </div>
                 </div>
             </div>
